b9972b050da94050ada14fcfc89e9d8597a1132b
mola1129 <ms.mola1129@gmail.com>
2019-12-28 00:09:42 +0900

    Add to do components.

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-card height="70px">
+      <v-card-title>{{ title }}</v-card-title>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ToDoItem',
+  props: ['title'],
+};
+</script>
+
+<style scoped>
+
+</style>
