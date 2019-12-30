a716bdebf9638d6da5a7b9025b2f366ab7764415
mola1129 <ms.mola1129@gmail.com>
2019-12-30 14:03:41 +0900

    Add a function for deleting todos.

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,7 +1,14 @@
 <template>
   <v-container>
-    <v-card height="70px">
-      <v-card-title>{{ title }}</v-card-title>
+    <v-card height="90px">
+      <v-row justify="space-around" align="center">
+        <v-col cols="11">
+          <v-card-title>{{ title }}</v-card-title>
+        </v-col>
+        <v-col cols="1">
+          <v-icon color="red lighten-2" @click="deleteItem()">far fa-times-circle</v-icon>
+        </v-col>
+      </v-row>
     </v-card>
   </v-container>
 </template>
@@ -10,6 +17,11 @@
 export default {
   name: 'ToDoItem',
   props: ['title'],
+  methods: {
+    deleteItem() {
+      this.$emit('delete');
+    },
+  },
 };
 </script>
 
