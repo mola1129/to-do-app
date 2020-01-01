b7a54f858ab11a359cea036d575965614dbf0a8b
mola1129 <ms.mola1129@gmail.com>
2020-01-01 17:33:18 +0900

    Add signup page.

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="signup">
-    <header>
-      <h1>Sign Up</h1>
-    </header>
-  </div>
+  <v-container class="fill-height" fluid>
+    <!-- style is for Vuetify's bug (https://github.com/vuetifyjs/vuetify/issues/9773) -->
+    <v-row align="center" justify="center" style="max-width: initial">
+      <v-col cols="12" sm="6" md="4">
+        <v-card class="elevation-12">
+          <v-card-text>
+            <v-form>
+              <v-text-field label="Username" name="login" type="text"/>
+              <v-text-field label="Password" name="password" type="password"/>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="red">Register</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
