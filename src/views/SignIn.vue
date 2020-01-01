2eb5ba233fc9890da487d98eac594af209000bea
mola1129 <ms.mola1129@gmail.com>
2020-01-01 21:22:39 +0900

    Connect to firestone. (SignUp)

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -6,16 +6,42 @@
           <v-card class="elevation-12">
             <v-card-text>
               <v-form>
-                <v-text-field label="Username" name="login" type="text"/>
-                <v-text-field label="Password" name="password" type="password"/>
+                <v-text-field label="Email" type="text" v-model="email"/>
+                <v-text-field label="Password" type="password" v-model="password"/>
               </v-form>
             </v-card-text>
             <v-card-actions>
               <v-spacer />
-              <v-btn color="primary">Login</v-btn>
+              <v-btn color="primary" @click="signIn">Login</v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
     </v-container>
 </template>
+
+<script>
+import firebase from 'firebase';
+
+export default {
+  name: 'SignIn',
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    signIn() {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        Console.log(errorCode);
+        Console.log(errorMessage);
+      });
+    },
+  },
+};
+
+</script>
