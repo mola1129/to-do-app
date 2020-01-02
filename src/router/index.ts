fbc7214467fe330bc38578bb95226b9770901f21
mola1129 <ms.mola1129@gmail.com>
2020-01-02 21:59:48 +0900

    Add login redirects.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import firebase from 'firebase';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import SignIn from '@/views/SignIn.vue';
+import SignUp from '@/views/SignUp.vue';
+import ToDo from '@/views/ToDo.vue';
 
 Vue.use(VueRouter);
 
@@ -13,25 +18,25 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: About,
   },
   {
     path: '/signin',
     name: 'signin',
-    component: () => import(/* webpackChunkName: "about" */ '../views/SignIn.vue'),
+    component: SignIn,
   },
   {
     path: '/signup',
     name: 'signup',
-    component: () => import(/* webpackChunkName: "about" */ '../views/SignUp.vue'),
+    component: SignUp,
   },
   {
     path: '/todo',
     name: 'todo',
-    component: () => import(/* webpackChunkName: "about" */ '../views/ToDo.vue'),
+    component: ToDo,
+    meta: {
+      requiresAuth: true,
+    },
   },
 ];
 
@@ -39,6 +44,24 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+// 未認証の場合はログイン画面へ
+router.beforeEach((to, from, next) => {
+  // 認証が必要なページがチェック
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log('認証中');
+        next();
+      } else {
+        console.log('未認証');
+        next({ path: '/signin' });
+      }
+    });
+  } else {
+    next(); // next() を常に呼び出すようにしてください!
+  }
 });
 
 export default router;
