86d742717ad61ebda498d17079548febde71ba12
mola1129 <ms.mola1129@gmail.com>
2020-01-03 22:31:35 +0900

    Split firebase settings.

---
+++ src/firebase.ts
@@ -0,0 +1,59 @@
+import firebase from 'firebase';
+import store from './store';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAJjaf-BBMrniE4m6zWVQJiUJmYcmN8jIg',
+  authDomain: 'to-do-app-d32ae.firebaseapp.com',
+  databaseURL: 'https://to-do-app-d32ae.firebaseio.com',
+  projectId: 'to-do-app-d32ae',
+  storageBucket: 'to-do-app-d32ae.appspot.com',
+  messagingSenderId: '778884966366',
+  appId: '1:778884966366:web:66db148437bcaaa78b1f4f',
+  measurementId: 'G-GZYBNBWG0Y',
+};
+
+export default {
+  init() {
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  },
+  loginWithEmail(email:string, password:string) {
+    firebase.auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(
+        (user) => {
+          console.log(`${user} sign in.`);
+        },
+      ).catch(
+        (e) => {
+          console.error(e.message);
+          console.error(e.code);
+        },
+      );
+  },
+  logout() {
+    firebase.auth().signOut().then(
+      (user) => {
+        console.log(`${user} sign out.`);
+      },
+    ).catch(
+      (e) => {
+        console.error(e.message);
+        console.error(e.code);
+      },
+    );
+  },
+  onAuth() {
+    firebase.auth().onAuthStateChanged((user) => {
+      // eslint-disable-next-line no-param-reassign
+      // @ts-ignore
+      // eslint-disable-next-line no-param-reassign
+      user = user || {};
+      store.commit('onAuthStateChanged', user);
+      // @ts-ignore
+      store.commit('onUserStatusChanged', !!user.uid);
+    });
+  },
+};
