0526fc008f36adcbc98fde08b5fee14601fc5d63
mola1129 <ms.mola1129@gmail.com>
2020-01-03 22:53:05 +0900

    Add NavBar components.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <div class="d-flex align-center">
+      <router-link to="/">
+        <v-img
+          alt="Mola1129 Logo"
+          class="shrink mr-2"
+          contain
+          :src="require('@/assets/mola.png')"
+          transition="scale-transition"
+          width="40"
+        />
+      </router-link>
+    </div>
+    <v-spacer/>
+    <auth-button/>
+  </v-app-bar>
+</template>
+
+<script>
+import AuthButton from '@/components/AuthButton.vue';
+
+export default {
+  name: 'NavBar',
+  components: { AuthButton },
+};
+</script>
