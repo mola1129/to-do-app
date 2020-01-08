770ac5aa7ea1cc16aa39812248c92faadabe16c4
mola1129 <ms.mola1129@gmail.com>
2020-01-08 21:54:12 +0900

    Update login page.

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -2,21 +2,27 @@
     <v-container class="fill-height" fluid>
       <!-- style is for Vuetify's bug (https://github.com/vuetifyjs/vuetify/issues/9773) -->
       <v-row align="center" justify="center" style="max-width: initial">
-        <v-col cols="12" sm="6" md="4">
-          <v-card class="elevation-12">
+        <v-col cols="12" sm="8" md="4" lg="4">
+          <v-card class="elevation-1 pa-3">
             <v-card-text>
+            <div class="layout column align-center">
+              <v-img src="../assets/mola2.png" height="120px" width="120px"/>
+              <h1 class="ma-3">To Do App</h1>
+            </div>
               <v-form>
                 <v-text-field label="Email" type="text" v-model="email"/>
                 <v-text-field label="Password" type="password" v-model="password"/>
               </v-form>
             </v-card-text>
             <v-card-actions>
-              <v-spacer />
-              <v-btn color="primary" @click="signIn">
-                <div v-if="!this.isWaiting">Login</div>
-                <v-progress-circular v-else color="white" indeterminate>
-                </v-progress-circular>
-              </v-btn>
+                <v-btn icon>
+                  <v-icon color="red">fab fa-google</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon color="light-blue">fab fa-twitter</v-icon>
+                </v-btn>
+                <v-spacer />
+                <v-btn color="primary" @click="signIn" :loading="isLoading">Login</v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -33,16 +39,18 @@ export default {
     return {
       email: '',
       password: '',
-      isWaiting: false,
+      isLoading: false,
     };
   },
   methods: {
     signIn() {
-      this.isWaiting = true;
+      this.isLoading = true;
       Firebase.loginWithEmail(this.email, this.password).then(() => {
-        this.$router.push('/todo');
+        setTimeout(() => {
+          this.$router.push('/todo');
+        }, 800);
       }).catch((e) => {
-        this.isWaiting = false;
+        this.isLoading = false;
         console.error('新規登録して下さい');
       });
     },
