742fdd8ed36286890d707e00409682c665b728cf
mola1129 <ms.mola1129@gmail.com>
2020-01-17 02:24:15 +0900

    Add SignInCard component.

---
+++ src/components/organisms/SignInCard.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="sign-in-card">
+    <v-card class="elevation-1 pa-3">
+      <v-card-text>
+        <div class="layout column align-center">
+          <v-img
+            src="@/assets/mola.png"
+            height="120px"
+            width="120px"
+          />
+          <h1 class="ma-4">
+            To Do App
+          </h1>
+        </div>
+        <v-alert
+          type="error"
+          v-if="!isExist"
+        >
+          The email or password is incorrect.
+        </v-alert>
+        <v-form v-model="valid">
+          <v-text-field
+            label="Email"
+            type="text"
+            :rules="emailRules"
+            v-model="email"
+          />
+          <v-text-field
+            label="Password"
+            type="password"
+            :rules="passwordRules"
+            v-model="password"
+          />
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          icon
+          @click="signInWithGoogle"
+        >
+          <v-icon color="red">
+            fab fa-google
+          </v-icon>
+        </v-btn>
+        <v-spacer/>
+        <v-btn
+          class="white--text"
+          color="green"
+          @click="signIn"
+          :loading="isLoading"
+          :disabled="!valid"
+        >
+          Sign In
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import Firebase from '@/firebase';
+
+export default {
+  name: 'SignInCard',
+  data() {
+    return {
+      email: '',
+      password: '',
+      isLoading: false,
+      isExist: true,
+      valid: true,
+      emailRules: [
+        v => !!v || '',
+      ],
+      passwordRules: [
+        v => !!v || '',
+      ],
+    };
+  },
+  methods: {
+    signIn() {
+      this.isLoading = true;
+      Firebase.loginWithEmail(this.email, this.password).then(() => {
+        setTimeout(() => {
+          this.$router.push('/todo');
+        }, 800);
+      }).catch((e) => {
+        this.isExist = false;
+        this.isLoading = false;
+      });
+    },
+    signInWithGoogle() {
+      Firebase.loginWithGoogle().then(() => {
+        this.$router.push('/todo');
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -15,60 +15,7 @@
           md="4"
           lg="4"
         >
-          <v-card class="elevation-1 pa-3">
-            <v-card-text>
-              <div class="layout column align-center">
-                <v-img
-                  src="@/assets/mola.png"
-                  height="120px"
-                  width="120px"
-                  />
-                <h1 class="ma-4">
-                  To Do App
-                </h1>
-              </div>
-              <v-alert
-                type="error"
-                v-if="!isExist"
-              >
-                The email or password is incorrect.
-              </v-alert>
-              <v-form v-model="valid">
-                <v-text-field
-                  label="Email"
-                  type="text"
-                  :rules="emailRules"
-                  v-model="email"
-                />
-                <v-text-field
-                  label="Password"
-                  type="password"
-                  :rules="passwordRules"
-                  v-model="password"
-                />
-              </v-form>
-            </v-card-text>
-            <v-card-actions>
-                <v-btn
-                  icon
-                  @click="signInWithGoogle"
-                >
-                  <v-icon color="red">
-                    fab fa-google
-                  </v-icon>
-                </v-btn>
-                <v-spacer/>
-                <v-btn
-                  class="white--text"
-                  color="green"
-                  @click="signIn"
-                  :loading="isLoading"
-                  :disabled="!valid"
-                >
-                  Sign In
-                </v-btn>
-            </v-card-actions>
-          </v-card>
+          <SignInCard />
         </v-col>
       </v-row>
     </v-container>
@@ -76,41 +23,12 @@
 
 <script>
 import Firebase from '@/firebase';
+import SignInCard from '@/components/organisms/SignInCard.vue';
 
 export default {
   name: 'SignIn',
-  data() {
-    return {
-      email: '',
-      password: '',
-      isLoading: false,
-      isExist: true,
-      valid: true,
-      emailRules: [
-        v => !!v || '',
-      ],
-      passwordRules: [
-        v => !!v || '',
-      ],
-    };
-  },
-  methods: {
-    signIn() {
-      this.isLoading = true;
-      Firebase.loginWithEmail(this.email, this.password).then(() => {
-        setTimeout(() => {
-          this.$router.push('/todo');
-        }, 800);
-      }).catch((e) => {
-        this.isExist = false;
-        this.isLoading = false;
-      });
-    },
-    signInWithGoogle() {
-      Firebase.loginWithGoogle().then(() => {
-        this.$router.push('/todo');
-      });
-    },
+  components: {
+    SignInCard,
   },
 };
 
