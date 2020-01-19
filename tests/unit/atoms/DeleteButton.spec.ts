45abdcf4ad951c588dd942005e34d554524ecf6c
mola1129 <ms.mola1129@gmail.com>
2020-01-19 19:27:12 +0900

    Add method tests.

--- tests/unit/atoms/DeleteButton.spec.ts
+++ tests/unit/atoms/DeleteButton.spec.ts
@@ -2,11 +2,23 @@ import { shallowMount } from '@vue/test-utils';
 import DeleteButton from '@/components/atoms/DeleteButton.vue';
 
 describe('DeleteButton', () => {
-  it('@click=onClickDeleteButton', () => {
-    const mock = jest.fn();
-    const wrapper = shallowMount(DeleteButton);
-    wrapper.setMethods({ onClickDeleteButton: mock });
-    wrapper.find('.delete-button').trigger('click');
-    expect(mock).toHaveBeenCalled();
+  describe('methods', () => {
+    // メソッドの実行
+    it('onClickDeleteButton', () => {
+      const wrapper = shallowMount(DeleteButton);
+      (wrapper.vm as any).onClickDeleteButton();
+      expect(wrapper.emitted('clickDeleteButton')).toBeTruthy();
+    });
+  });
+
+  describe('template', () => {
+    // クリック時にメソッドが呼ばれているか
+    it('@click=onClickDeleteButton', () => {
+      const mock = jest.fn();
+      const wrapper = shallowMount(DeleteButton);
+      wrapper.setMethods({ onClickDeleteButton: mock });
+      wrapper.find('.delete-button').trigger('click');
+      expect(mock).toHaveBeenCalled();
+    });
   });
 });
